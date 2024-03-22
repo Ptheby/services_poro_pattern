a12fe1a555e35bc5009c7c9f48ba059af4e93859
Paul Theby <paultheby@gmail.com>
2024-03-21 19:12:11 -0500

    corrected serviceCOntract module

--- app/services/service_contract.rb
+++ app/services/service_contract.rb
@@ -1,24 +1,11 @@
-module UserService
-    module Base
-      def self.create_user(params)
-        user = User.new(params)
-  
-        begin
-           # are there any db/model errors?
-          user.save!
-        rescue ActiveRecord::RecordInvalid => exception
-          # return an error instance
-          return ServiceContract.error(user.errors.full_messages) unless user.valid?
-        end
-  
-        ServiceContract.success(user)
-      end
-  
-      def self.filter_users(params)
-        users = User.all
-        users = users.where(name: params[:name]) if params[:name].present?
-        users = users.where(email: params[:email]) if params[:email].present?
-        ServiceContract.success(users)
-      end
+module ServiceContract
+
+    def self.success(payload)
+      OpenStruct.new({ success?: true, payload: payload, errors: nil })
     end
+  
+    def self.error(errors)
+      OpenStruct.new({ success?: false, payload: nil, errors: errors })
+    end
+  
   end
